44f715a9d12de21e6c83a05b4f00a2d78f39532e
Artem <ironfather22@gmail.com>
2026-07-01 10:35:43 +0300

    Add low priority detection

--- priority.py
+++ priority.py
@@ -18,6 +18,14 @@ def get_priority(issue):
         "degraded"
     ]
 
+    low_priority_keywords = [
+        "minor",
+        "alignment",
+        "slightly reduced",
+        "monitor",
+        "working"
+    ]
+
     for keyword in high_priority_keywords:
         if keyword in issue_lower:
             return "HIGH"
@@ -25,5 +33,9 @@ def get_priority(issue):
     for keyword in normal_priority_keywords:
         if keyword in issue_lower:
             return "NORMAL"
+        
+    for keyword in low_priority_keywords:
+        if keyword in issue_lower:
+            return "LOW"
         
     return "CHECK REQUIRED"
