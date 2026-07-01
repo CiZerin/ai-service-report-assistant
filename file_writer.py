9325fa4afad7053d75694dade942e78f2da70fe9
Artem <ironfather22@gmail.com>
2026-07-01 09:33:13 +0300

    Create reports folder automatically

--- file_writer.py
+++ file_writer.py
@@ -1,9 +1,14 @@
+import os
 import json
 
+
 def save_report(filename, report):
+    os.makedirs("reports", exist_ok=True)
+
     with open(filename, "w") as file:
         file.write(report)
 
 def save_json_report(filename, report):
+    os.makedirs("reports", exist_ok=True)
     with open(filename, "w") as file:
         json.dump(report, file, indent=4)
